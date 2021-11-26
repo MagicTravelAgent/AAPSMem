4f1a6e8defbb1b1378861d38f3b668cc829c2c45
Jort.Gutter <jort.gutter@ru.nl>
2021-11-26 12:29:57 +0100

    added main functionality except actual image generation

--- code/card.py
+++ code/card.py
@@ -8,11 +8,42 @@ class Card:
         self.front_image = front_image
         self.hidden = True
 
+    def get_size(self):
+        """
+        Returns the size of the card in pixels. The height and width are the same.
+        :return:
+        """
+        return self.size
+
+    def hide(self):
+        """
+        Hides the card and returns the back image
+        :return:
+        """
+        self.hidden = True
+        return self.back_image
+
+    def show(self):
+        """
+        Reveals the card and returns the front image
+        :return:
+        """
+        self.hidden = False
+        return self.front_image
+
     def flip(self):
+        """
+        flips the card and returns the corresponding image
+        :return:
+        """
         self.hidden = not self.hidden
         return self.get_image()
 
     def get_image(self):
+        """
+        returns the currently facing image
+        :return:
+        """
         if self.hidden:
             logging.debug(f'The card is face down')
             return self.back_image
--- code/generate_cards.py
+++ code/generate_cards.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import logging
 import sys
+import random
+from card import Card
 
 
 class CardGenerator:
@@ -11,6 +13,7 @@ class CardGenerator:
         self.card_size = 100
         self.n_cards = 0
         self.cards = []
+        self.back_image = self.generate_back_image()
         pass
 
     def set_difficulty(self, difficulty):
@@ -21,14 +24,30 @@ class CardGenerator:
         self.card_size = card_size
         logging.debug(f'the size of the cards has been set to {card_size}x{card_size} pixels')
 
+    def generate_back_image(self):
+        img = None
+        return img
+
+    def generate_front_image(self, background):
+        shapes = 5
+        shapes += self.difficulty*2
+        img = np.array([np.full((self.card_size, self.card_size), col) for col in background])
+        return img
+
     def generate_cards(self, n_cards):
+        background = [random.random()*255 for i in range(3)]
         if n_cards % 2:
             print(f'ERROR: You need to specify an even number of cards! {n_cards} is not an even number, silly!', file=sys.stderr)
             return []
         self.n_cards = n_cards
 
         for i in range(int(n_cards/2)):
-            new_card = cv2.full()
+            new_card = Card(
+                size=self.card_size,
+                back_image=self.back_image,
+                front_image=self.generate_front_image(background)
+            )
+            self.cards.append(new_card)
         logging.info(f'{n_cards} cards have been generated')
         return self.cards
 
@@ -44,4 +63,5 @@ cg = CardGenerator()
 cg.set_difficulty(2)
 cg.set_size(200)
 cg.generate_cards(4)
+cg.flip_card(2)
 
